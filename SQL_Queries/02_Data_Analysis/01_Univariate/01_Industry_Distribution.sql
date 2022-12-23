c33771fdc2f9fac1057862e3c5ae5fba08a976c5
abhimanyu <32466050+absmdt@users.noreply.github.com>
2022-12-23 19:24:41 +0530

    Update 01_Industry_Distribution.sql

--- SQL_Queries/02_Data_Analysis/01_Univariate/01_Industry_Distribution.sql
+++ SQL_Queries/02_Data_Analysis/01_Univariate/01_Industry_Distribution.sql
@@ -1,17 +1,17 @@
 -- Industry distribution
 -- Checking distinct industry names
 SELECT DISTINCT(industry)
-FROM unicorn
+FROM unicorn_cos
 ORDER BY industry ASC;
 
 -- Counting how many distinct industries there are
 SELECT COUNT(DISTINCT(industry))
-FROM unicorn;
+FROM unicorn_cos;
 -- There are 15 different industries in the dataset
 
 -- Checking the distribution of unicorns companies across each industry field
 SELECT industry, COUNT(industry)
-FROM unicorn
+FROM unicorn_cos
 WHERE industry != 'Other'
 GROUP BY industry
 ORDER BY COUNT(industry) DESC;
@@ -19,5 +19,5 @@ ORDER BY COUNT(industry) DESC;
 
 -- Checking companies within the 'Other' industry field
 SELECT *
-FROM unicorn
+FROM unicorn_cos
 WHERE industry = 'Other';
